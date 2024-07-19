f5227e81fce01f990724d76576fc34c2601b18b9
shashidharummaneni <145983395+shashidharummaneni@users.noreply.github.com>
2024-07-19 08:39:04 +0530

    Update BakeryLoaves1(1).py

--- BakeryLoaves1(1).py
+++ BakeryLoaves1(1).py
@@ -3,4 +3,11 @@ ol = float(input("Enter the number of day-old loaves purchased: "))
 print("Regular price: Rs.185.00")
 print("Amount for new loaves:",fl*185)
 print("Amount for day-old loaves:",ol*(185*0.6))
-print("Total amount: Rs.",fl*185 + ol*(185*0.6))
+print("Total amount: Rs.",fl*185 + ol*(185*0.6))                              # OUTPUT
+                                                                              #Enter the number of fresh loaves purchased: 5
+                                                                              #Enter the number of day-old loaves purchased: 3
+                                                                              #Regular price: Rs.185.00
+                                                                              #Amount for new loaves: 925.0
+                                                                              #Amount for day-old loaves: 333.0
+                                                                              #Total amount: Rs. 1258.0
+
